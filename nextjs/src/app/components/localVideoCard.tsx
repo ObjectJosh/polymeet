2374bf0e7076274c1670ce940d4f4a2ec8499b5a
Miguel Villa Floran <miguel.villafloran@gmail.com>
2024-06-04 17:47:00 -0700

    Clean up stuff

--- nextjs/src/app/components/localVideoCard.tsx
+++ nextjs/src/app/components/localVideoCard.tsx
@@ -2,63 +2,40 @@ import { getLocalStream } from './webrtc';
 import React, { useRef, useState, useEffect } from 'react';
 
 const LocalVideoCard = () => {
-  const [playing, setPlaying] = useState<Boolean>(false);
+    /* Whether video is streamed */
+    const [audio, setAudio] = useState<Boolean>(false);
 
-  // controls the current stream value
-  const [stream, setStream] = useState(null);
-  
-  // controls if audio/video is on or off (seperately from each other)
-  const [audio, setAudio] = useState(true);
-  const [video, setVideo] = useState(true);
+    /* Whether audio is streamed */
+    const [video, setVideo] = useState<Boolean>(false);
 
-  // controls the video DOM element
-  const webcamVideo = useRef();
+    /* Whether a valid video source exists */
+    const videoRef = useRef<HTMLVideoElement>(null);
 
-  // get the user's media stream
-  const startStream = async () => {
-    let newStream = await getLocalStream().then((newStream) => {
-        webcamVideo.current.srcObject = newStream;
-        setStream(stream);
-      });
-    setPlaying(true);
-  };
-  
-   // stops the user's media stream
-   const stopStream = () => {
-    stream.getTracks().forEach((track) => track.stop());
-    setPlaying(false);
-  };
-
-  // enable/disable audio tracks in the media stream
-  const toggleAudio = () => {
-      setAudio(!audio);
-      stream.getAudioTracks()[0].enabled = audio;
-  };
-
-  // enable/disable video tracks in the media stream
-  const toggleVideo = () => {
-    setVideo(!video);
-    stream.getVideoTracks()[0].enabled = !video;
-  };
-
-  useEffect(() => {
-    startStream();
-    return () => {
-      console.log('Component will unmount');
+    const setVideoRef = async (mediaStream: MediaStream | null) => {
+      const video = videoRef.current;
+      if (video) {
+        video.srcObject = mediaStream;
+        video.autoplay = video;
+        video.controls = false;
+        video.muted = audio;
+      }
     };
-  }, []); 
-  return (
-    <>
-      <video ref={webcamVideo} autoPlay={video} muted={true} playsInline></video>
-      <button
-          onClick={playing ? stopStream : startStream}>
-          Start webcam
-      </button>
 
-      <button onClick={toggleAudio}>Toggle Sound</button>
-      <button onClick={toggleVideo}>Toggle Video</button>
-    </>
-  );
+    useEffect(() => {
+      async function fetchData() {
+        setVideoRef(await getLocalStream());
+      }
+      fetchData();
+    }, []);
+    return (
+        <>
+            <video ref={videoRef} autoPlay={video} muted={true} playsInline></video>
+            {/* <button onClick={playing ? stopStream : startStream}>Start webcam</button>
+
+            <button onClick={toggleAudio}>Toggle Sound</button>
+            <button onClick={toggleVideo}>Toggle Video</button> */}
+        </>
+    );
 };
 
 export default LocalVideoCard;
