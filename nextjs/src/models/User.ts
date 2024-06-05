dd95484b15d80c05d445bb57e0b3c3e3604c041f
ZealSV <54489933+ZealSV@users.noreply.github.com>
2024-06-04 22:52:23 -0700

    changed userschema

--- nextjs/src/models/User.ts
+++ nextjs/src/models/User.ts
@@ -1,33 +1,70 @@
 import mongoose from 'mongoose';
 
 export interface User extends mongoose.Document {
-    first_name: string;
-    last_name: string;
+    userId: string;
     email: string;
+    password: string;
+    firstName: string;
+    lastName: string;
+    major?: string;
+    year?: string;
+    hobbies?: string[];
+    classes?: string[];
+    clubs?: string[];
+    blockedUserIds?: string[];
+    createDate: Date;
+    lastLoginDate?: Date;
 }
 
-/* UserSchema will correspond to a collection in your MongoDB database. */
 const UserSchema = new mongoose.Schema<User>({
-    first_name: {
-        /* The name of the user */
-
+    userId: {
         type: String,
-        required: [true, 'Please provide a first for this user.'],
-        maxlength: [60, 'Name cannot be more than 60 characters'],
-    },
-    last_name: {
-        /* The name of the user */
-
-        type: String,
-        required: [true, 'Please provide a last for this user.'],
-        maxlength: [60, 'Name cannot be more than 60 characters'],
+        required: [true, 'Please provide a user ID for this user.'],
+        unique: true,
     },
     email: {
-        /* The email of the user */
-
         type: String,
-        required: [true, 'Please provide a email for this user.'],
+        required: [true, 'Please provide an email for this user.'],
         maxlength: [60, 'Email cannot be more than 60 characters'],
+        unique: true,
+    },
+    firstName: {
+        type: String,
+        required: [true, 'Please provide a first name for this user.'],
+        maxlength: [60, 'First name cannot be more than 60 characters'],
+    },
+    lastName: {
+        type: String,
+        required: [true, 'Please provide a last name for this user.'],
+        maxlength: [60, 'Last name cannot be more than 60 characters'],
+    },
+    major: {
+        type: String,
+        maxlength: [100, 'Major cannot be more than 100 characters'],
+    },
+    year: {
+        type: String,
+        maxlength: [20, 'Year cannot be more than 20 characters'],
+    },
+    hobbies: {
+        type: [String],
+    },
+    classes: {
+        type: [String],
+    },
+    clubs: {
+        type: [String],
+    },
+    blockedUserIds: {
+        type: [String],
+    },
+    createDate: {
+        type: Date,
+        required: [true, 'Please provide a creation date for this user.'],
+        default: Date.now,
+    },
+    lastLoginDate: {
+        type: Date,
     },
 });
 
