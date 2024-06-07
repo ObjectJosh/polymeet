78f57fa97d8f215b73cecaa9dac6b97e0437085b
Miguel Villa Floran <miguel.villafloran@gmail.com>
2024-06-07 04:40:08 -0700

    Stuff

--- nextjs/src/app/components/Room/index.tsx
+++ nextjs/src/app/components/Room/index.tsx
@@ -4,9 +4,6 @@ import { VideoConfig, getLocalStream } from '../webrtc';
 import { useMemo, useState, useRef, useEffect } from 'react';
 import { io } from 'socket.io-client';
 import { v4 } from 'uuid';
-import Messages from './Messages';
-import Options from './Options';
-import VideoCard from '../localVideoCard';
 import { Videocam, VideocamOff, Mic, MicOff } from '@mui/icons-material';
 import { IconButton } from '@mui/material';
 
@@ -21,12 +18,20 @@ const servers = {
 
 export default function Room() {
     const server = 'https://polymeet-7137e04975b4.herokuapp.com/';
-    // const [socket, setSocket] = useState(io(server));
-    // const partnerRef = useRef(null);
+    const [socket, setSocket] = useState(io(server));
 
-    // const [userId, setUserId] = useState<String>(v4());
-    // const [remoteStream, setRemoteStream] = useState<MediaStream | null>(null);
+    const [userId, setUserId] = useState<String>(v4());
+    const [remoteStream, setRemoteStream] = useState<MediaStream | null>(null);
     const [localStream, setLocalStream] = useState<MediaStream | null>(null);
+
+    const remoteVideoRef = useRef<HTMLVideoElement>(null);
+    const setRemoteVideoRef = async (mediaStream: MediaStream | null) => {
+        const retVid = remoteVideoRef.current;
+        if (retVid) {
+            retVid.srcObject = mediaStream;
+            retVid.controls = false;
+        }
+    };
     const localVideoRef = useRef<HTMLVideoElement>(null);
     const setLocalVideoRef = async (mediaStream: MediaStream | null) => {
         const vid = localVideoRef.current;
@@ -41,30 +46,119 @@ export default function Room() {
         audio: true,
     });
 
+    useEffect(() => {
+        initialize();
+    }, []);
+
     const localVideoHandler = async () => {
         getLocalStream(videoConfig.video, videoConfig.audio).then((stream) => {
-            console.log('Stream:', stream);
             setLocalVideoRef(stream);
         });
     };
 
     async function initialize() {
+        let peer: RTCPeerConnection;
+        let joinTimeout: any;
+        let offerSent = false;
+        let answerSent = false;
         localVideoHandler();
+
+        // Signals to the server that a new user just joined the call
+        console.log('Joining room...');
+        socket.emit('joinRoom', { userId });
+
+        socket.on('waiting', () => {
+            console.log('Waiting 20 seconds for someone to join...');
+            joinTimeout = setTimeout(() => {
+                console.log('No one joined, searching for a room...');
+                socket.emit('joinRoom', { userId });
+            }, 20000000);
+
+            socket.on('user_joined', async () => {
+                console.log('Someone joined! timeout canceled.');
+                clearTimeout(joinTimeout);
+
+                peer = new RTCPeerConnection(servers);
+
+                peer.ontrack = (e) => {
+                    console.log('here');
+                    setRemoteStream(e.streams[0]);
+                };
+
+                localStream.getTracks().forEach((track: any) => {
+                    peer.addTrack(track, localStream);
+                });
+
+                peer.onicecandidate = (e) => {
+                    if (e.candidate && !offerSent) {
+                        offerSent = true;
+                        console.log(e.candidate);
+                        socket.emit('offer', {
+                            offerDescription: peer.localDescription,
+                            candidate: e.candidate,
+                        });
+                    }
+                };
+
+                const offerDescription = await peer.createOffer();
+                peer.setLocalDescription(offerDescription);
+            });
+        });
+
+        socket.on('reset', () => {
+            setRemoteStream(null);
+            socket.emit('joinRoom', { userId });
+            offerSent = false;
+            answerSent = false;
+        });
+
+        socket.on('process-offer', async ({ offerDescription, candidate }) => {
+            peer = new RTCPeerConnection(servers);
+
+            peer.ontrack = (e) => {
+                console.log('here');
+                setRemoteStream(e.streams[0]);
+            };
+
+            localStream.getTracks().forEach((track: any) => {
+                peer.addTrack(track, localStream);
+            });
+
+            peer.onicecandidate = (e) => {
+                if (e.candidate && !answerSent) {
+                    answerSent = true;
+                    socket.emit('answer', {
+                        answerDescription: peer.localDescription,
+                        candidate: e.candidate,
+                    });
+                }
+            };
+
+            await peer.setRemoteDescription(offerDescription);
+            peer.addIceCandidate(candidate);
+
+            const answerDescription = await peer.createAnswer();
+            peer.setLocalDescription(answerDescription);
+        });
+
+        socket.on('process-answer', async ({ answerDescription, candidate }) => {
+            await peer.setRemoteDescription(answerDescription);
+            peer.addIceCandidate(candidate);
+        });
     }
 
     useEffect(() => {
-        console.log('Video state:', videoConfig.video);
-        console.log('Audio state:', videoConfig.audio);
         if (videoConfig.video || videoConfig.audio) {
             localVideoHandler();
         } else {
             setLocalVideoRef(null);
         }
-    }, [videoConfig.video, videoConfig.audio]);
+    }, [videoConfig]);
 
     useEffect(() => {
-        initialize();
-    }, []);
+        console.log(remoteStream);
+        setRemoteVideoRef(remoteStream);
+    }, [remoteStream]);
 
     const toggleStream = async () => {
         console.log('Starting stream...');
@@ -99,7 +193,8 @@ export default function Room() {
                 }}
             >
                 <p style={{ position: 'absolute', left: 20, top: 20 }}>Them</p>
-                {/* <video
+                <video
+                    ref={remoteVideoRef}
                     autoPlay={true}
                     muted={true}
                     playsInline
@@ -109,7 +204,7 @@ export default function Room() {
                         borderRadius: '10px',
                         objectFit: 'cover',
                     }}
-                ></video> */}
+                ></video>
             </div>
             <div
                 style={{
@@ -129,7 +224,7 @@ export default function Room() {
                 <video
                     ref={localVideoRef}
                     autoPlay={true}
-                    muted={false}
+                    muted={true}
                     playsInline
                     style={{
                         width: '100%',
@@ -147,49 +242,6 @@ export default function Room() {
                     </IconButton>
                 </div>
             </div>
-            {/* <div className='video-container'>
-                <video 
-                    id="partner" 
-                    ref={partnerRef}
-                    muted={videoConfig.deafened} 
-                    style={{
-                        display: `${!remoteStream ? 'none': 'block'}`
-                    }}
-                />
-                <Options
-                    videoConfig={videoConfig}
-                    setVideoConfig={setVideoConfig}
-                    socket={socket}
-                />
-            </div>
-            {
-                !remoteStream ? 
-                null
-                :
-                <>
-                    <Messages socket={socket} id={userId} messageBoxOn={videoConfig.messageBoxOn} />
-                </>
-            }
-            
-            <video 
-                ref={ref} 
-                id="me"
-                onMouseDown={(e:any) => {
-                    setVideoClicked(true);
-                }}
-
-                onMouseMove={(e:any) => {
-                    if(!ref.current || !videoClicked) return;
-                    console.log(e.clientX);
-                    const v:any = ref.current;
-                    v.style.left = (parseInt(e.clientX) - 100)+"px";
-                    v.style.top = (parseInt(e.clientY) - 50)+"px";
-                }} 
-
-                onMouseUp={(e:any) => {
-                    setVideoClicked(false);
-                }}
-            /> */}
         </div>
     );
 }
