21129d043660f61d0ef089493daac3f15d8fe429
Miguel Villa Floran <miguel.villafloran@gmail.com>
2024-06-07 03:43:17 -0700

    Save for later

--- nextjs/src/app/components/localVideoCard.tsx
+++ nextjs/src/app/components/localVideoCard.tsx
@@ -1,55 +1,39 @@
-import { getLocalStream } from './webrtc';
-import React, { useRef, useState, useEffect } from 'react';
+import React, { MutableRefObject } from 'react';
 import { Videocam, VideocamOff, Mic, MicOff } from '@mui/icons-material';
 import { IconButton } from '@mui/material';
+import { VideoConfig } from '../webrtc';
 
-const LocalVideoCard = () => {
-    /* Whether video is streamed */
-    const [audio, setAudio] = useState<boolean>(false);
+interface VideoConfigProps {
+    videoConfig: VideoConfig;
+    setVideoConfig: (videoConfig: VideoConfig) => void;
+    videoRef: MutableRefObject<HTMLVideoElement | null>;
+    setVideoRef: (mediaStream: MediaStream | null) => void;
+}
 
-    /* Whether audio is streamed */
-    const [video, setVideo] = useState<boolean>(false);
-
-    /* Whether a valid video source exists */
-    const videoRef = useRef<HTMLVideoElement>(null);
-
-    /* The WebRTC peer-to-peer connection */
-    const peerConnection = useRef<RTCPeerConnection | null>(null);
-
-    const setVideoRef = async (mediaStream: MediaStream | null) => {
-        const newVideo = videoRef.current;
-        if (newVideo) {
-            newVideo.srcObject = mediaStream;
-            newVideo.controls = false;
-        }
-    };
-
+export default function LocalVideoCard(props: VideoConfigProps): JSX.Element {
     const startStream = async () => {
-        setVideo(true);
+        console.log('Starting stream...');
+        props.setVideoConfig({
+            ...props.videoConfig,
+            video: true,
+        });
     };
 
     // stops the user's media stream
     const stopStream = async () => {
-        setVideo(false);
+        props.setVideoConfig({
+            ...props.videoConfig,
+            video: false,
+        });
     };
 
     // enable/disable audio tracks in the media stream
     const toggleAudio = () => {
-        setAudio(!audio);
+        props.setVideoConfig({
+            ...props.videoConfig,
+            audio: !props.videoConfig.audio,
+        });
     };
-
-    useEffect(() => {
-        console.log('Video state:', video);
-        console.log('Audio state:', audio);
-        if (video || audio) {
-            getLocalStream(video, audio).then((stream) => {
-                console.log('Stream:', stream);
-                setVideoRef(stream);
-            });
-        } else {
-            setVideoRef(null);
-        }
-    }, [video, audio]);
 
     return (
         <>
@@ -69,7 +53,7 @@ const LocalVideoCard = () => {
             >
                 <p style={{ position: 'absolute', left: 20, top: 20 }}>You</p>
                 <video
-                    ref={videoRef}
+                    ref={props.videoRef}
                     autoPlay={true}
                     muted={false}
                     playsInline
@@ -81,16 +65,14 @@ const LocalVideoCard = () => {
                     }}
                 ></video>
                 <div style={{ position: 'absolute', bottom: 10, left: 10, display: 'flex', gap: '10px' }}>
-                    <IconButton onClick={video ? stopStream : startStream} color='primary'>
-                        {video ? <Videocam /> : <VideocamOff />}
+                    <IconButton onClick={props.videoConfig.video ? stopStream : startStream} color='primary'>
+                        {props.videoConfig.video ? <Videocam /> : <VideocamOff />}
                     </IconButton>
                     <IconButton onClick={toggleAudio} color='primary'>
-                        {audio ? <Mic /> : <MicOff />}
+                        {props.videoConfig.audio ? <Mic /> : <MicOff />}
                     </IconButton>
                 </div>
             </div>
         </>
     );
-};
-
-export default LocalVideoCard;
+}
